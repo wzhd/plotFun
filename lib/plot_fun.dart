aa4532f3928fcd3b81b72e53aaa682ccefaa4586
wzhd <wzhd@outlook.de>
2014-02-24 13:12:42 +0800

    Fix fineness control

--- lib/plot_fun.dart
+++ lib/plot_fun.dart
@@ -21,7 +21,7 @@ import 'dart:svg' show SvgElement;
     String equation5;
     int xmin = -10;
     int xmax = 10;
-    int fineness = 100;
+    double fineness = 100.0;
     bool showEquation2 = false;
     bool showEquation3 = false;
     bool showEquation4 = false;
