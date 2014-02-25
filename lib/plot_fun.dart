540eb48d37dda0f56034c05c54857836bc609b03
wzhd <wzhd@outlook.de>
2014-02-25 16:23:15 +0800

    Correct indentation in controller

--- lib/plot_fun.dart
+++ lib/plot_fun.dart
@@ -7,37 +7,37 @@ import 'package:plotfun/xkcd_plot.dart';
 @NgController(
     selector: '[plot-fun]',
     publishAs: 'ctrl')
-  class PlotFunController {
-    //variables
-    String title;
-    String xlabel;
-    String ylabel;
-    List<Map<String, String>> equations = new List<Map<String, String>>();
-    int xmin = -10;
-    int xmax = 10;
-    double fineness = 100.0;
-    String warning;
+class PlotFunController {
+  //variables
+  String title;
+  String xlabel;
+  String ylabel;
+  List<Map<String, String>> equations = new List<Map<String, String>>();
+  int xmin = -10;
+  int xmax = 10;
+  double fineness = 100.0;
+  String warning;
 
-    XkcdPlot ploter = new XkcdPlot(document.querySelector('#plot'));
+  XkcdPlot ploter = new XkcdPlot(document.querySelector('#plot'));
 
-    PlotFunController() {
-      equations.add({'expression': ''});
-    }
-
-    void morePlots() {
-      equations.add({'expression': ''});
-    }
-
-    void drawGraph() {
-      Map param = {
-        'title': title,
-        'xlabel': xlabel,
-        'ylabel': ylabel,
-        'xmin': xmin,
-        'xmax': xmax,
-        'fineness': fineness,
-        'xlim': [xmin - (xmax - xmin) / 16,
-          xmax + (xmax - xmin) / 16] };
-      warning = ploter.drawGraphEquation(equations, param);
-    }
+  PlotFunController() {
+    equations.add({'expression': ''});
   }
+
+  void morePlots() {
+    equations.add({'expression': ''});
+  }
+
+  void drawGraph() {
+    Map param = {
+      'title': title,
+      'xlabel': xlabel,
+      'ylabel': ylabel,
+      'xmin': xmin,
+      'xmax': xmax,
+      'fineness': fineness,
+      'xlim': [xmin - (xmax - xmin) / 16,
+        xmax + (xmax - xmin) / 16] };
+    warning = ploter.drawGraphEquation(equations, param);
+  }
+}
