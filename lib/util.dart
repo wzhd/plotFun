f80a5ef5b31d895484592077481e8f7137b502aa
wzhd <wzhd@outlook.de>
2014-02-26 16:02:40 +0800

    Remove no longer used function extent from lib/util.dart

--- lib/util.dart
+++ lib/util.dart
@@ -1,29 +1,6 @@
 import 'dart:math' as Math;
 import 'dart:collection' show Queue;
 
-//find the min and max of data
-List<num> extent(data, accessor) {
-  num access(d) {
-    if (d is num)
-      return d;
-    else
-      return accessor(d);
-  }
-  num max = double.NEGATIVE_INFINITY;
-  num min = double.INFINITY;
-  for (int i = 0; i < data.length; i++) {
-    var curr = data[i];
-    if (access(curr) < min) {
-      min = access(curr);
-    }
-    if (access(curr) > max) {
-      max = access(curr);
-    }
-  }
-  List<num> ext = [min, max];
-  return ext;
-}
-
 class LinearScale {
   List<num> domain = [0, 1];
   List<num> range = [0, 1];
